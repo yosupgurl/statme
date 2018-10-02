85ebbcbe3bd7229768d917aa56d765fa3eeacbb4
yosupgurl <benabitbul@hotmail.com>
2018-10-02 17:32:19 -0400

    added blizzard api gem, created a few methods and did testing, couldn't get wrapper gem to work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,24 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+	skip_before_action :authenticate_user!, only: [:home]
 
-  def home
-  end
+	def home
+	end
+
+	def dashboard
+		setblizzardapi	
+		diablo
+	end
+
+	def diablo
+		client = Battlenet.D3Client
+	end
+
+	def setblizzardapi
+		Battlenet.configure do |config|
+			config.api_key = 'mk5r9un4n5asm6paxjhm3zuxz25yencx'
+			config.region  = :us
+		end
+	end
+	private :setblizzardapi
+
 end
