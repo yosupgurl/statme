583f76b64de5ac6d1067c27c5969bdf80302375c
yosupgurl <benabitbul@hotmail.com>
2018-10-04 08:00:49 -0400

    changed the gem for battlenet api and deleted old code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,21 +4,12 @@ class PagesController < ApplicationController
 	def home
 	end
 
-	def dashboard
-		setblizzardapi	
-		diablo
-	end
-
 	def diablo
-		client = Battlenet.D3Client
 	end
 
 	def setblizzardapi
-		Battlenet.configure do |config|
-			config.api_key = 'mk5r9un4n5asm6paxjhm3zuxz25yencx'
-			config.region  = :us
-		end
 	end
+	
 	private :setblizzardapi
 
 end
